18a48101ac223082be69fd1d3b2f1e02222f7628
Neo Nie <nihgwu@live.com>
2022-03-31 22:42:13 +0100

    chore: update react-runner

--- preset.js
+++ preset.js
@@ -6,28 +6,7 @@ function managerEntries(entry = []) {
   return [...entry, require.resolve("./dist/esm/preset/manager")];
 }
 
-async function configWebpack(config, { presets }) {
-  const version = await presets.apply("webpackVersion");
-  if (version === '5') {
-    config.module.rules.push({
-      test: /\.mjs$/,
-      resolve: {
-        fullySpecified: false,
-      }
-    });
-  } else {
-    config.module.rules.push({
-      test: /\.mjs$/,
-      type: "javascript/auto",
-    });
-  }
-
-  return config;
-}
-
 module.exports = {
-  managerWebpack: configWebpack,
-  webpackFinal: configWebpack,
   managerEntries,
   config,
 };
